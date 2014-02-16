a39d394154d6839aa248e8fe92c7766e74d70e3c
ntphuc <thienphuc0510@gmail.com>
2014-02-16 08:11:11 +0100

    delete folders

--- Android_MVC/src/com/phucn/mvc/model/ServerPath.java
+++
@@ -1,6 +0,0 @@
-package com.phucn.mvc.model;
-
-public class ServerPath {
-	public static final String SERVER_PATH = "https://www.googleapis.com/youtube/v3/";
-	public static final String KEY = "AIzaSyAvHd2GLwcUs8igGksFVz4qYfrmTT76Jko";
-}
